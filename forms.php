10e5378628d386a00cabc86af2554546f2a692bd
Infomulas <109298894+Infomulas@users.noreply.github.com>
2022-07-14 22:12:55 -0300

    Add files via upload

---
+++ forms.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Formulário</title>
+</head>
+<body>
+<form action="" method="post">
+
+    <label>Id:</label>
+    <input type="text" name="Id" placeholder="Digite o Id">
+    <br>
+
+    <label for="">Id fornecedor:</label>
+    <input type="text" name="Id Fornecedor" placeholder="Digite o Id">
+    <br>
+
+    <label for="">Id categoria: </label>
+    <input type="text" name="Id categoria" placeholder="Digite o id">
+    <br>
+
+    <label for="">Produto</label>
+    <input type="text" name="Produto" placeholder="Insira o produto">
+    <br>
+
+    <label for="">Foto produto</label>
+    <input type="text" name="foto" placeholder="Foto do produto">
+    <br>
+
+    <label for="">Descrição</label>
+    <input type="text" name="Descrição" placeholder="Insira a descrição">
+    <br>
+
+    <label for="">Valor</label>
+    <input type="text" name="Valor" placeholder="Insira o Valor">
+    <br>
+
+    <label for="">Estoque</label>
+    <input type="text" name="Estoque" placeholder="Insira o Estoque">
+    <br>
+
+    <label for="">Estoque Min.</label>
+    <input type="text" name="Estoque Min." placeholder="Insira o Estoque Min.">
+    <br>
+    
+    <input type="submit" value="Inserir">
+
+
+    </form>
+</body>
+</html>
+
+
+
+
+
+
+
+
+<?php
+
+
+
+
+
+
+
+?>
