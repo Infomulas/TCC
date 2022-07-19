845273ff3dd4f38153f54f3a2c272d143f761d64
Infomulas <109298894+Infomulas@users.noreply.github.com>
2022-07-19 11:27:38 -0300

    Update conexao.php

--- conexao.php
+++ conexao.php
@@ -1,6 +1,6 @@
 <?php 
 
-$mysqli = new mysqli("localhost", "root","","comprebem_db");
+$mysqli = new mysqli("localhost", "root","","comprebem_db", 3307);
 if (mysqli_connect_errno()){
     echo "Erro ao conectar ao banco de dados: ".mysqli_connect_error();
     exit();
